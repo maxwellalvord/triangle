33fa5066963541f58b23758822bcd2d77300f4c8
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-02 17:31:48 -0700

    BUGFIX: fixed logic and calling to be correct

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@
 function triCalc(sideOne, sideTwo, sideThree) {
   /* logic for each if/else still needs to be adjusted, 
   don't forget to use && / ||to mean 'and' / || */
-  if (sideOne === sideTwo === sideThree) {
+  if ((sideOne === sideTwo) && (sideThree === sideOne)) {
     return "equilaterial";
-  } else if ((sideOne === sideTwo) != sideThree) {
-    return "isosceles";
-  } else if ((sideOne + sideTw) <= sideThree) {
+  } else if ((sideOne + sideTwo) <= (sideThree)) {
     return "Not a Triangle";
   }  else if ((sideOne + sideThree) <= sideTwo) {
     return "Not a Triangle";
   } else if ((sideThree + sideTwo) <= sideOne) {
     return "Not a Triangle";
+  } else if ((sideOne === sideTwo) || (sideThree === sideOne) || (sideTwo === sideThree)) {
+    return "isosceles";
   } else {
     return "scalene";
   }
@@ -27,9 +27,9 @@ $(document).ready(function() {
   $("form#triCalc").submit(function(event) {
 
     //adjusted the ID names to match the ones on the html (input1/input2/input3)
-    const number1 = parseInt($("input1").val());
-    const number2 = parseInt($("input2").val());
-    const number3 = parseInt($("input3").val());
+    const number1 = parseInt($("input#input1").val());
+    const number2 = parseInt($("input#input2").val());
+    const number3 = parseInt($("input#input3").val());
     // updated the arguments below to match the variable names above
     let result = triCalc(number1, number2, number3);
     
